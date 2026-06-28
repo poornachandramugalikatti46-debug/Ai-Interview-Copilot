79abe86b1bcc054324ab91461c53e4477034f14a
Poornachandra <poornachandramugalikatti46@gmail.com>
2026-06-28 11:57:49 +0530

    add user model and auth DB

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 /* ================= REGISTER ================= */
 router.post("/register", async (req, res) => {
