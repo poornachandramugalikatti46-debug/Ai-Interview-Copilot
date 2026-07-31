c22ac1a17a028bd79438350e31b86a59157bd285
Purnachandra P Mugalikatti <poornachandramugalikatti46@gmail.com>
2026-07-31 17:30:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: "https://ai-interview-copilot-frontend-ccwr.onrender.com",
     credentials: true,
   })
 );
@@ -75,7 +75,7 @@ const startServer = (port) => {
   const server = http.createServer(app);
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:3000"],
+      origin: "http://ai-interview-copilot-frontend-ccwr.onrender.com",
       methods: ["GET", "POST"],
       credentials: true,
     },
